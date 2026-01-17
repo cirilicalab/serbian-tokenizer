5b80036b58acc3b916a7e1c4f2114378d2283d9c
Goku <goku@pitomioblak.com>
2026-01-17 23:32:29 +0100

    Normalize digits

--- src/srbtok/srb_tokenizer.py
+++ src/srbtok/srb_tokenizer.py
@@ -19,6 +19,16 @@ trans_dict = {
     '’' : "'",      # different single quotes to ascii single quote
     '‚' : "'",
     '‘' : "'",
+    '0' : '0',      # all digits become 0 so all numbers are treated based on number of digits only (needed for sentence tokenizer)
+    '1' : '0',
+    '2' : '0',
+    '3' : '0',
+    '4' : '0',
+    '5' : '0',
+    '6' : '0',
+    '7' : '0',
+    '8' : '0',
+    '9' : '0',
 }
 trans_table = str.maketrans(trans_dict)
 
